475b317fc7bd0cb483991fa49f2f7db48811b760
gabzai <gabriel.zaid@univ-st-etienne.fr>
2020-07-11 10:15:39 +0200

    first commit

---
+++ AES_HD/cnn_architecture.py
@@ -0,0 +1,270 @@
+import os
+import sys
+import time
+import pickle
+import numpy as np
+import tensorflow as tf
+
+from keras.models import Model
+from keras.layers import Flatten, Dense, Input, Conv1D, AveragePooling1D, BatchNormalization, Activation
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.utils import to_categorical
+from sklearn import preprocessing
+from exploit_pred import *
+
+### Scripts based on ASCAD-v1 github : https://github.com/ANSSI-FR/ASCAD
+
+class Logger(object):
+    def __init__(self, name_file):
+        self.terminal = sys.stdout
+        self.log = open(str(name_file)+".log", "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass    
+
+
+def check_file_exists(file_path):
+    if os.path.exists(file_path) == False:
+        print("Error: provided file path '%s' does not exist!" % file_path)
+        sys.exit(-1)
+    return
+
+def shuffle_data(profiling_x,label_y):
+    l = list(zip(profiling_x,label_y))
+    random.shuffle(l)
+    shuffled_x,shuffled_y = list(zip(*l))
+    shuffled_x = np.array(shuffled_x)
+    shuffled_y = np.array(shuffled_y)
+    return (shuffled_x, shuffled_y)
+
+# Naive implementation
+def loss_sca(score_vector, alpha_value=10, nb_class=256):
+
+    # Rank loss function
+    def ranking_loss_sca(y_true, y_pred):
+
+        alpha = K.constant(alpha_value, dtype='float32')
+
+        # Batch_size initialization
+        y_true_int = K.cast(y_true, dtype='int32')
+        batch_s = K.cast(K.shape(y_true_int)[0],dtype='int32')
+
+        # Indexing the training set (range_value = (?,))
+        range_value = K.arange(0, batch_s, dtype='int64')
+
+        # Get rank and scores related to the secret key (rank_sk = (?,))
+        values_topk_logits, indices_topk_logits = tf.nn.top_k(score_vector, k=nb_class, sorted=True) # values_topk_logits = shape(?, nb_class) ; indices_topk_logits = shape(?, nb_class)
+        rank_sk = tf.where(tf.equal(K.cast(indices_topk_logits, dtype='int64'), tf.reshape(K.argmax(y_true_int), [tf.shape(K.argmax(y_true_int))[0], 1])))[:,1] + 1 # Index of the correct output among all the hypotheses (shape(?,))
+        score_sk = tf.gather_nd(values_topk_logits, K.concatenate([tf.reshape(range_value, [tf.shape(values_topk_logits)[0], 1]), tf.reshape(rank_sk-1, [tf.shape(rank_sk)[0], 1])])) # Score of the secret key (shape(?,))
+
+        # Ranking Loss initialization
+        loss_rank = 0
+
+        for i in range(nb_class):
+
+            # Score for each key hypothesis (s_i_shape=(?,))
+            s_i = tf.gather_nd(values_topk_logits, K.concatenate([tf.reshape(range_value, [tf.shape(values_topk_logits)[0], 1]), i*tf.ones([tf.shape(values_topk_logits)[0], 1], dtype='int64')]))
+
+            # Indicator function identifying when (i == secret key)
+            indicator_function = tf.ones(batch_s) - (K.cast(K.equal(rank_sk-1, i), dtype='float32') * tf.ones(batch_s))
+
+            # Logistic loss computation
+            logistic_loss = K.log(1 + K.exp(- alpha * (score_sk - s_i)))/K.log(2.0)
+
+            # Ranking Loss computation
+            loss_rank = tf.reduce_sum((indicator_function * logistic_loss))+loss_rank
+
+        return loss_rank/(K.cast(batch_s, dtype='float32'))
+
+    #Return the ranking loss function
+    return ranking_loss_sca
+
+### CNN network
+def cnn_architecture(input_size=1250, learning_rate=0.00001, alpha_value=10, classes=256, rkl_loss=True):
+
+    # Personal design
+    input_shape = (input_size,1)
+    img_input = Input(shape=input_shape)
+
+    # 1st convolutional block
+    x = Conv1D(2, 1, kernel_initializer='he_uniform', activation='selu', padding='same', name='block1_conv1')(img_input)
+    x = BatchNormalization()(x)
+    x = AveragePooling1D(2, strides=2, name='block1_pool')(x)
+
+    x = Flatten(name='flatten')(x)
+
+    # Classification layer
+    x = Dense(2, kernel_initializer='he_uniform', activation='selu', name='fc1')(x)
+
+    # Logits layer
+    score_layer = Dense(classes, activation=None, name='score')(x)
+    predictions = Activation('softmax')(score_layer)
+
+    # Create model
+    inputs = img_input
+    model = Model(inputs, predictions, name='aes_hd')
+    optimizer = Adam(lr=learning_rate)
+
+    if(rkl_loss==True):
+        model.compile(loss=loss_sca(score_layer, nb_class=classes, alpha_value=alpha_value), optimizer=optimizer, metrics=['accuracy'])
+    else:
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+        
+    return model
+
+
+#### Training model
+def train_model(X_profiling, Y_profiling, X_test, Y_test, model, save_file_name, epochs=150, batch_size=100):
+    check_file_exists(os.path.dirname(save_file_name))
+
+    # Save model every epoch
+    save_model = ModelCheckpoint(save_file_name)
+
+    # Get the input layer shape
+    input_layer_shape = model.get_layer(index=0).input_shape
+
+    # Sanity check
+    if input_layer_shape[1] != len(X_profiling[0]):
+        print("Error: model input shape %d instead of %d is not expected ..." % (input_layer_shape[1], len(X_profiling[0])))
+        sys.exit(-1)
+    
+    Reshaped_X_profiling, Reshaped_X_test  = X_profiling.reshape((X_profiling.shape[0], X_profiling.shape[1], 1)),X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
+    callbacks=[save_model] 
+          
+    history = model.fit(x=Reshaped_X_profiling, y=to_categorical(Y_profiling, num_classes=256), validation_data=(Reshaped_X_test, to_categorical(Y_test, num_classes=256)), batch_size=batch_size, verbose = 1, epochs=epochs, callbacks=callbacks)
+    return history
+
+
+#################################################
+#################################################
+
+#####            Initialization            ######
+
+#################################################
+#################################################
+
+# Our folders
+root = "./"
+AESHD_data_folder = root+"AES_HD_dataset/"
+AESHD_trained_models_folder = root+"AESHD_trained_models/"
+history_folder = root+"training_history/"
+predictions_folder = root+"model_predictions/"
+
+# Hyperparameters
+nb_epochs = 20
+batch_size = 256
+input_size = 1250
+learning_rate = 1e-3
+nb_prof_traces = 45000
+alpha = 1
+nb_traces_attacks = 1500
+nb_attacks = 100
+real_key = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+# Load the profiling traces
+(X_profiling, Y_profiling), (X_attack, Y_attack), (ciphertext_profiling, ciphertext_attack) = (np.load(AESHD_data_folder + 'profiling_traces_AES_HD.npy'), np.load(AESHD_data_folder + 'profiling_labels_AES_HD.npy')), (np.load(AESHD_data_folder + 'attack_traces_AES_HD.npy'), np.load(AESHD_data_folder + 'attack_labels_AES_HD.npy')), (np.load(AESHD_data_folder + 'profiling_ciphertext_AES_HD.npy'), np.load(AESHD_data_folder + 'attack_ciphertext_AES_HD.npy'))
+
+# Shuffle data
+(X_profiling, Y_profiling) = shuffle_data(X_profiling, Y_profiling)
+
+#Traces Scaling (between 0 and 1)
+scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+X_profiling = scaler.fit_transform(X_profiling)
+X_attack = scaler.transform(X_attack)
+
+X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))
+
+X_profiling = X_profiling.astype('float32')
+X_attack = X_attack.astype('float32')
+
+counter = 0
+
+# Log Initialization
+sys.stdout = Logger(root+"logs/AES_HD_nbprof"+str(nb_prof_traces))
+
+while(counter < 2):
+
+    #################################################
+    #################################################
+
+    ####                Training               ######
+
+    #################################################
+    #################################################
+
+    # Choose your model
+    if(counter==0):
+        model = cnn_architecture(input_size=input_size, learning_rate=learning_rate, alpha_value=alpha, rkl_loss=False)
+        model_name="AES_HD_cce_nbprof"+str(nb_prof_traces)
+
+    else:
+        model = cnn_architecture(input_size=input_size, learning_rate=learning_rate, alpha_value=alpha, rkl_loss=True)
+        model_name="AES_HD_rkl_nbprof"+str(nb_prof_traces)+"_alpha"+str(alpha)
+
+    print('\n Model name = '+model_name)
+
+    print("\n############### Starting Training #################\n")
+
+    start = time.time()
+
+    # Record the metrics
+    history = train_model(X_profiling[:nb_prof_traces], Y_profiling[:nb_prof_traces], X_profiling[45000:], Y_profiling[45000:], model, AESHD_trained_models_folder + model_name, epochs=nb_epochs, batch_size=batch_size)
+    
+    end = time.time()
+    print('Training Time = %d'%(end-start))
+
+    print("\n############### Training Done #################\n")
+
+    # Save the metrics
+    with open(history_folder + 'history_' + model_name, 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
+
+    #################################################
+    #################################################
+
+    ####               Prediction              ######
+
+    #################################################
+    #################################################
+
+    print("\n############### Starting Predictions #################\n")
+
+    predictions = model.predict(X_attack)
+
+    print("\n############### Predictions Done #################\n")
+
+    np.save(predictions_folder + 'predictions_' + model_name +'.npy', predictions)
+
+
+    #################################################
+    #################################################
+
+    ####            Perform attacks            ######
+
+    #################################################
+    #################################################
+
+    print("\n############### Starting Attack on Test Set #################\n")
+
+    avg_rank = perform_attacks(nb_traces_attacks, predictions, nb_attacks, ciph=ciphertext_attack, key=real_key, byte=0, filename=model_name)
+
+    print("\n Nt_GE = ")
+    print(np.where(avg_rank==0))
+    print("\n")
+
+    print("\n############### Attack on Test Set Done #################\n")
+
+    K.clear_session()
+    del model
+
+    counter+=1
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Ranking-Loss-SCA
+# Ranking Loss: Maximizing the Success Rate in Deep Learning Side-Channel Analysis
+Anonymous Submission
+
+Each dataset is composed of the following scripts and repositories:
+- <b>cnn_architecture.py</b>: provides the script in order to train the model introduced in the article,
+- <b>exploit_pred.py</b>: computes the evolution of the right key and saves the resulted picture,
+- <b>(Optionnal) clr.py</b>: computes the One-Cycle Policy (see "<a href='https://arxiv.org/abs/1708.07120'>Super-Convergence: Very Fast Training of Neural Networks Using Large Learning Rates
+</a>" and "<a href='https://arxiv.org/abs/1803.09820'>A disciplined approach to neural network hyper-parameters: Part 1 -- learning rate, batch size, momentum, and weight decay</a>,
+- <b>"training_history"</b>: contains information related to the loss function, the accuracy
+- <b>"model_predictions"</b>: contains information related to the model predictions,
+- <b>"fig"</b>: contains the figure related to the rank evolution.
+
+The trace sets were obtained from publicly databases: 
+- <b>AES_HD dataset</b>: https://github.com/AESHD/AES_HD_Dataset
+- <b>ASCAD</b>: https://github.com/ANSSI-FR/ASCAD
+
+## Raw data files hashes
+The zip file SHA-256 hash value is:
+<hr>
+
+**AES_HD/AES_HD_dataset.zip:**
+`00a3d02f01bae8c4fcefda33e3d1adb57bed0509ded3cdcf586e213b3d87e41b`
+
+<hr>
+
+**ASCAD/Desync0/ASCAD_dataset.zip:**
+`5f5924e2d0beca5b57fbc48ace137dbb2fe12dd03976aa38f4a699ab21e966b0`
+
+**ASCAD/Desync50/ASCAD_dataset.zip:**
+`9bf704727390a73cf67d3952bc2cacef532b0b62e55f85d615edaa6cd8521f51`
+
+**ASCAD/Desync100/ASCAD_dataset.zip:**
+`2d803db27e58fec3d805cd3cf039b303cad1e0c9ea7a8102a07020bd07113cd9`
+
+<hr>
